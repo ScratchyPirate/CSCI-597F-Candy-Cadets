34579f7221febd4a00cd128da34867d79904d53f
ScratchyPirate <john_sbur@yahoo.com>
2026-02-28 15:15:12 -0800

    added prototype wheel control

--- packages/my_package/src/wheel_control_node.py
+++ packages/my_package/src/wheel_control_node.py
@@ -29,21 +29,89 @@ class WheelControlNode(DTROS):
         # construct publisher
         self._publisher = rospy.Publisher(wheels_topic, WheelsCmdStamped, queue_size=1)
 
-        # construct subscriber for other nodes to use 
-        # self._subscriber = rospy.Subscriber('chatter', String, self.callback)
+        # construct subscriber for other nodes to use to control the wheel
+        self._subscriber = rospy.Subscriber('wheel_control', String, self.run)
 
-
-    def run(self, velocity_left: float, velocity_right: float, time: float):
+    def parse_yaml(raw_input: str):
         """
-            This method sets the robot's wheels to move 
+          Parses a YAML formatted string, following the format:
+          ---
+          <argument>: <value>
+          <argument>: <value>
+          ---
+        """
+        output = {} # Create an empty dictionary as output
+
+        lines = raw_input.split("\n") # Split our yaml based on lines
+
+        for line in lines: # for each of the lines
+            variable = line.split(":") # store the argument and its value
+            output[variable[0].strip(" -")] = variable[1].strip(" -") # put the argument and its value into our output dictionary
+
+        return output # return the dictionary containing all arguments and their corresponding values
+
+    def duration_from_distance(distance: float, velocity):
+        """Given a distance in cm and using velocity
+        determine the length of time needed to reach that distance"""
+        # TODO
+        return -1
+    
+    def duration_from_angle(distance: float, left_vel, right_vel):
+        """Given an angle and left and right wheel velocities
+        determine the length of time needed to turn that amount"""
+        # TODO
+        return -1
+
+    def run(self, movement_type: std_msgs.msg.String):
+        """
+            This method sets the robot's wheels to move
+
+            Expected format movement_type:{type}\narg:{argument}
+              Where 
+              {type} is either 'forward' or 'turn' 
+              {argument} is the angle to rotate the robot by when type is turn or the distance
+                  to drive the robot forward in centimeters when type is forward
+
         """
         
-        # publish 10 messages every second (10 Hz)
-        rate = rospy.Rate(0.1)
-        message = WheelsCmdStamped(vel_left=self._vel_left, vel_right=self._vel_right)
-        while not rospy.is_shutdown():
+        """
+        arguments dictionary has the following form:
+        {"movement_type": "forward", "argument": 3.14159}
+        """
+        arguments = self.parse_yaml(movement_type)
+        
+        match arguments["movement_type"]: # check the value of movement_type
+            case "forward":
+                left_wheel_vel = self._vel_left
+                right_wheel_vel = self._vel_right
+                duration = self.duration_from_distance(arguments["argument"], left_wheel_vel) # find the duration to move based on velocity and distance
+            case "turn":
+                """
+                if turning, move the wheels at the velocity 
+                needed to attain the specified angle of rotation.
+                """
+                if arguments["argument"] > 0: # if positive angle, turn clockwise
+                    left_wheel_vel = self._vel_left
+                    right_wheel_vel = -self._vel_right
+                else: # if negative angle, turn counter-clockwise
+                    left_wheel_vel = -self._vel_left
+                    right_wheel_vel = self._vel_right
+                duration = self.duration_from_angle(arguments["argument"], left_wheel_vel, right_wheel_vel) # find the duration to move based on velocity and distance
+                
+            # default case if no other case matches
+            case _:
+                print(f"Incorrect argument given to run. Format should be the following:",
+                      f"movement_type:<type>",
+                      f"arg:<argument>", sep="\n")
+                return 
+        
+        # publish 10 messages every second (10 Hz) for duration of time
+        rate = rospy.Rate(0.1) # 10 Hz
+        message = WheelsCmdStamped(vel_left=left_wheel_vel, vel_right=right_wheel_vel) #move wheels at quarter of a rotation
+        while not rospy.is_shutdown(): # TODO: how to make sure this runs for the given duration?
             self._publisher.publish(message)
             rate.sleep()
+
 
     def on_shutdown(self):
         stop = WheelsCmdStamped(vel_left=0, vel_right=0)
